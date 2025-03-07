fd7ed2b9bfd9a794766d7ac9521a2690f17f8592
Sarker5523 <piyalsarkerrifat@hotmail.com>
2025-03-07 06:07:37 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
-
-// Function to shuffle an array
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-// Function to get all item ids
-function getAllItemIds(data) {
-    return data.map(item => item.id);
-}
+const path = require('path');
 
 // Serve the static files (your HTML file)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the main page with the video player
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-// Route to get the data with randomized suggestions
-app.get('/data', (req, res) => {
-    // Read the JSON file
-    const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-
-    // Get all item ids
-    const allItemIds = getAllItemIds(data);
-
-    // Add randomized suggestions to each item
-    const dataWithSuggestions = data.map(item => {
-        const suggestions = shuffleArray(allItemIds.filter(id => id !== item.id));
-        return { ...item, suggestions };
-    });
-
-    res.json(dataWithSuggestions);
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
